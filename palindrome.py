29c132c4e8844cf443cd3cfa63ae9a7da12fbb08
NITYA BARWI <95531972+nityabarwi@users.noreply.github.com>
2024-04-18 21:38:56 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+def isPalindrome(x):
+  if x < 0 or (x % 10 == 0 and x != 0):
+    return False
+  reverse = 0
+  original = x
+  while x > 0:
+    digit = x % 10
+    reverse = reverse * 10 + digit
+    x //= 10
+  return original == reverse
