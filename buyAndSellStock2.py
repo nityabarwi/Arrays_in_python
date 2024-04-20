6dba811f1d132cebd68e0836a94e84a99658b4d2
NITYA BARWI <95531972+nityabarwi@users.noreply.github.com>
2024-04-20 07:36:19 +0530

    Create buyAndSellStock2.py

---
+++ buyAndSellStock2.py
@@ -0,0 +1,9 @@
+def maxProfit(prices):
+  profit = 0
+  for i in range(1, len(prices)):
+    if prices[i] > prices[i - 1]:
+      profit += prices[i] - prices[i - 1]
+  return profit
+
+prices1 = [7,1,5,3,6,4]
+print(maxProfit(prices1))
